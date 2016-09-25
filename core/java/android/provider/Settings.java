38b6c855d9b2ea314d7717139d96ce4915884ea7
BeansTown106 <nbenis106@gmail.com>
2016-09-24 19:58:49 -0700

    QS: Add toggle for fancy QS animation [1/2]
    Change-Id: I330fd5963a04ee6ab893cfc7093749cb76ff5596

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4378,6 +4378,12 @@ public final class Settings {
         public static final String QS_COLUMNS_LANDSCAPE = "qs_columns_landscape";
 
         /**
+         * Quick Settings nougat animation
+         * @hide
+         */
+        public static final String QS_FANCY_ANIMATION = "qs_fancy_animation";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
