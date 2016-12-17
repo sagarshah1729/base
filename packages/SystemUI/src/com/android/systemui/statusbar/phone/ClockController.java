jfdesignnet/platform_frameworks_base

Frameworks: statusbar clock date customization (1/2)
This is a partial pick for clock date options with some modifications

Original commit:
https://github.com/SlimRoms/frameworks_base/commit/39545c535c0d0d6857dd769eeae8bda73955d3b1

packages/SystemUI/src/com/android/systemui/statusbar/phone/ClockController.java
@@ -29,6 +29,8 @@ public class ClockController {
 
     private int mClockLocation;
     private int mAmPmStyle;
+    private int mClockDateStyle;
+    private int mClockDateDisplay;
     private int mIconTint = Color.WHITE;
 
     class SettingsObserver extends ContentObserver {
@@ -42,6 +44,12 @@ public class ClockController {
                     Settings.System.STATUS_BAR_AM_PM), false, this);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.STATUS_BAR_CLOCK), false, this);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.STATUS_BAR_CLOCK_DATE_DISPLAY), false, this);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.STATUS_BAR_CLOCK_DATE_STYLE), false, this);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.STATUS_BAR_CLOCK_DATE_FORMAT), false, this);
             updateSettings();
         }
 
@@ -94,6 +102,8 @@ public class ClockController {
         mActiveClock = getClockForCurrentLocation();
         mActiveClock.setVisibility(View.VISIBLE);
         mActiveClock.setAmPmStyle(mAmPmStyle);
+        mActiveClock.setClockDateDisplay(mClockDateDisplay);
+        mActiveClock.setClockDateStyle(mClockDateStyle);
 
         setClockAndDateStatus();
         setTextColor(mIconTint);
@@ -106,6 +116,10 @@ public class ClockController {
                 Settings.System.STATUS_BAR_AM_PM, Clock.AM_PM_STYLE_GONE);
         mClockLocation = Settings.System.getInt(
                 resolver, Settings.System.STATUS_BAR_CLOCK, STYLE_CLOCK_RIGHT);
+        mClockDateDisplay = Settings.System.getInt(resolver,
+                Settings.System.STATUS_BAR_CLOCK_DATE_DISPLAY, Clock.CLOCK_DATE_DISPLAY_GONE);
+        mClockDateStyle = Settings.System.getInt(resolver,
+                Settings.System.STATUS_BAR_CLOCK_DATE_STYLE, Clock.CLOCK_DATE_STYLE_REGULAR);
         updateActiveClock();
     }
 

sagarshah1729/base

Change-Id: I96869609f7a4c543ccf152d3bbf63339186196b1

packages/SystemUI/src/com/android/systemui/statusbar/phone/ClockController.java
@@ -32,6 +32,8 @@ public class ClockController {
     private int mAmPmStyle;
     private int mClockLocation;
     private boolean mShowSeconds;
+    private int mClockDateStyle;
+    private int mClockDateDisplay;
 
     private int mIconTint = Color.WHITE;
 
@@ -50,6 +52,15 @@ public class ClockController {
                     false, this, UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.STATUS_BAR_CLOCK_SECONDS),
+                    false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.STATUS_BAR_CLOCK_DATE_DISPLAY),
+                    false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.STATUS_BAR_CLOCK_DATE_STYLE),
+                    false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.STATUS_BAR_CLOCK_DATE_FORMAT),
                     false, this, UserHandle.USER_ALL);
             updateSettings();
         }
@@ -105,6 +116,8 @@ public class ClockController {
         mActiveClock.setVisibility(View.VISIBLE);
         mActiveClock.setAmPmStyle(mAmPmStyle);
         mActiveClock.setShowSeconds(mShowSeconds);
+        mActiveClock.setClockDateDisplay(mClockDateDisplay);
+        mActiveClock.setClockDateStyle(mClockDateStyle);
 
         setClockAndDateStatus();
         setTextColor(mIconTint);
@@ -122,6 +135,12 @@ public class ClockController {
         mShowSeconds = Settings.System.getIntForUser(resolver,
                 Settings.System.STATUS_BAR_CLOCK_SECONDS, 0,
                 UserHandle.USER_CURRENT) == 1;
+        mClockDateDisplay = Settings.System.getIntForUser(resolver,
+                Settings.System.STATUS_BAR_CLOCK_DATE_DISPLAY, Clock.CLOCK_DATE_DISPLAY_GONE,
+                UserHandle.USER_CURRENT);
+        mClockDateStyle = Settings.System.getIntForUser(resolver,
+                Settings.System.STATUS_BAR_CLOCK_DATE_STYLE, Clock.CLOCK_DATE_STYLE_REGULAR,
+                UserHandle.USER_CURRENT);
         updateActiveClock();
     }
 