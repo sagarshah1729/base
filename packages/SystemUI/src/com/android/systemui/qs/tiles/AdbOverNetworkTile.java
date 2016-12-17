DirtyUnicorns/android_frameworks_base

[QUICK SETTINGS] Add adb over network QS Tile
Change-Id: I553ed1c4b7237c5ecac8b69ab7a94259d8de1ceb

packages/SystemUI/src/com/android/systemui/qs/tiles/AdbOverNetworkTile.java
@@ -1,0 +1,128 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.qs.tiles;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.database.ContentObserver;
+import android.net.NetworkUtils;
+import android.net.Uri;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.os.UserHandle;
+import android.provider.Settings;
+import com.android.systemui.R;
+import com.android.systemui.qs.QSTile;
+import com.android.internal.logging.MetricsProto.MetricsEvent;
+
+import java.net.InetAddress;
+
+public class AdbOverNetworkTile extends QSTile<QSTile.BooleanState> {
+
+    @Override
+    public BooleanState newTileState() {
+        return new BooleanState();
+    }
+
+    @Override
+    protected void handleClick() {
+        Settings.Secure.putIntForUser(mContext.getContentResolver(),
+                Settings.Secure.ADB_PORT, getState().value ? -1 : 5555,
+                UserHandle.USER_CURRENT);
+    }
+
+    @Override
+    public Intent getLongClickIntent() {
+        return new Intent().setComponent(new ComponentName(
+            "com.android.settings", "com.android.settings.Settings$DevelopmentSettingsActivity"));
+    }
+
+    @Override
+    public CharSequence getTileLabel() {
+        return mContext.getString(R.string.quick_settings_adb_network);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.QUICK_SETTINGS;
+    }
+
+    @Override
+    protected void handleUpdateState(BooleanState state, Object arg) {
+        state.value = isAdbNetworkEnabled();
+        if (state.value) {
+            WifiManager wifiManager = (WifiManager) mContext.getSystemService(Context.WIFI_SERVICE);
+            WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+
+            if (wifiInfo != null) {
+                // if wifiInfo is not null, set the label to "hostAddress"
+                InetAddress address = NetworkUtils.intToInetAddress(wifiInfo.getIpAddress());
+                state.label = address.getHostAddress();
+            } else {
+                // if wifiInfo is null, set the label without host address
+                state.label = mContext.getString(R.string.quick_settings_network_adb_enabled_label);
+            }
+            state.icon = ResourceIcon.get(R.drawable.ic_qs_network_adb_on);
+        } else {
+            // Otherwise set the label and disabled icon
+            state.label = mContext.getString(R.string.quick_settings_network_adb_disabled_label);
+            state.icon = ResourceIcon.get(R.drawable.ic_qs_network_adb_off);
+        }
+    }
+
+    private boolean isAdbEnabled() {
+        return Settings.Secure.getInt(mContext.getContentResolver(),
+                Settings.Global.ADB_ENABLED, 0) > 0;
+    }
+
+    private boolean isAdbNetworkEnabled() {
+        return Settings.Secure.getInt(mContext.getContentResolver(),
+                Settings.Secure.ADB_PORT, 0) > 0;
+    }
+
+    public AdbOverNetworkTile(Host host) {
+        super(host);
+    }
+
+    private ContentObserver mObserver = new ContentObserver(mHandler) {
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+            refreshState();
+        }
+    };
+
+    @Override
+    public void destroy() {
+        mContext.getContentResolver().unregisterContentObserver(mObserver);
+    }
+
+    @Override
+    public void setListening(boolean listening) {
+        if (listening) {
+            mContext.getContentResolver().registerContentObserver(
+                    Settings.Secure.getUriFor(Settings.Secure.ADB_PORT),
+                    false, mObserver);
+            mContext.getContentResolver().registerContentObserver(
+                    Settings.Secure.getUriFor(Settings.Global.ADB_ENABLED),
+                    false, mObserver);
+        } else {
+            mContext.getContentResolver().unregisterContentObserver(mObserver);
+        }
+    }
+
+}

sagarshah1729/base

Signed-off-by: Sagar Shah <sagarshah2326@gmail.com>

packages/SystemUI/src/com/android/systemui/qs/tiles/AdbOverNetworkTile.java
@@ -1,0 +1,128 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.qs.tiles;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.database.ContentObserver;
+import android.net.NetworkUtils;
+import android.net.Uri;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.os.UserHandle;
+import android.provider.Settings;
+import com.android.systemui.R;
+import com.android.systemui.qs.QSTile;
+import com.android.internal.logging.MetricsProto.MetricsEvent;
+
+import java.net.InetAddress;
+
+public class AdbOverNetworkTile extends QSTile<QSTile.BooleanState> {
+
+    @Override
+    public BooleanState newTileState() {
+        return new BooleanState();
+    }
+
+    @Override
+    protected void handleClick() {
+        Settings.Secure.putIntForUser(mContext.getContentResolver(),
+                Settings.Secure.ADB_PORT, getState().value ? -1 : 5555,
+                UserHandle.USER_CURRENT);
+    }
+
+    @Override
+    public Intent getLongClickIntent() {
+        return new Intent().setComponent(new ComponentName(
+            "com.android.settings", "com.android.settings.Settings$DevelopmentSettingsActivity"));
+    }
+
+    @Override
+    public CharSequence getTileLabel() {
+        return mContext.getString(R.string.quick_settings_adb_network);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.COS_TILE;
+    }
+
+    @Override
+    protected void handleUpdateState(BooleanState state, Object arg) {
+        state.value = isAdbNetworkEnabled();
+        if (state.value) {
+            WifiManager wifiManager = (WifiManager) mContext.getSystemService(Context.WIFI_SERVICE);
+            WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+
+            if (wifiInfo != null) {
+                // if wifiInfo is not null, set the label to "hostAddress"
+                InetAddress address = NetworkUtils.intToInetAddress(wifiInfo.getIpAddress());
+                state.label = address.getHostAddress();
+            } else {
+                // if wifiInfo is null, set the label without host address
+                state.label = mContext.getString(R.string.quick_settings_network_adb_enabled_label);
+            }
+            state.icon = ResourceIcon.get(R.drawable.ic_qs_network_adb_on);
+        } else {
+            // Otherwise set the label and disabled icon
+            state.label = mContext.getString(R.string.quick_settings_network_adb_disabled_label);
+            state.icon = ResourceIcon.get(R.drawable.ic_qs_network_adb_off);
+        }
+    }
+
+    private boolean isAdbEnabled() {
+        return Settings.Secure.getInt(mContext.getContentResolver(),
+                Settings.Global.ADB_ENABLED, 0) > 0;
+    }
+
+    private boolean isAdbNetworkEnabled() {
+        return Settings.Secure.getInt(mContext.getContentResolver(),
+                Settings.Secure.ADB_PORT, 0) > 0;
+    }
+
+    public AdbOverNetworkTile(Host host) {
+        super(host);
+    }
+
+    private ContentObserver mObserver = new ContentObserver(mHandler) {
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+            refreshState();
+        }
+    };
+
+    @Override
+    public void destroy() {
+        mContext.getContentResolver().unregisterContentObserver(mObserver);
+    }
+
+    @Override
+    public void setListening(boolean listening) {
+        if (listening) {
+            mContext.getContentResolver().registerContentObserver(
+                    Settings.Secure.getUriFor(Settings.Secure.ADB_PORT),
+                    false, mObserver);
+            mContext.getContentResolver().registerContentObserver(
+                    Settings.Secure.getUriFor(Settings.Global.ADB_ENABLED),
+                    false, mObserver);
+        } else {
+            mContext.getContentResolver().unregisterContentObserver(mObserver);
+        }
+    }
+
+}